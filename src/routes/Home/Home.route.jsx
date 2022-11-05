45198b2f1462b92500be14231b126135597f9cef
rvitality <vital.renzjohn@gmail.com>
2022-11-05 22:44:52 +0800

    feat: add Home component that consists of Hero & HowItWorks components

---
+++ src/routes/Home/Home.route.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Hero from "../../components/Hero/Hero.component";
+import HowItWorks from "../../components/HowItWorks/HowItWorks.component";
+
+const Home = () => {
+    return (
+        <>
+            <Hero />
+            <HowItWorks />
+        </>
+    );
+};
+
+export default Home;
