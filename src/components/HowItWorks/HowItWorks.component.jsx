4700b9376ed4b77bdf1c10362b762101fecf6fba
rvitality <vital.renzjohn@gmail.com>
2022-11-05 22:48:53 +0800

    feat: add HowItWorks component

---
+++ src/components/HowItWorks/HowItWorks.component.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+
+import "./HowItWorks.styles.scss";
+
+import signupIcon from "../../assets/icons/signup.png";
+import approvedIcon from "../../assets/icons/approved.png";
+import orderIcon from "../../assets/icons/order.png";
+
+const HowItWorks = () => {
+    return (
+        <section className="how-it-works section-px section-py">
+            <div className="heading">
+                <h2 className="heading-tertiary">How it Works</h2>
+                <div className="heading-secondary">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima, vitae.
+                </div>
+            </div>
+
+            <div className="steps">
+                <div className="steps__item">
+                    <h3>Become a Member</h3>
+                    <div>
+                        <img src={signupIcon} alt="Signup Icon" />
+                        <div className="subtext">
+                            <p>
+                                Register for an account. Provide genuine information as they will be
+                                reviewed for approval.
+                            </p>
+                            <p className="notice">We don't share your personal information.</p>
+                        </div>
+                    </div>
+                </div>
+
+                <div className="steps__item">
+                    <h3>Place your Request</h3>
+                    <div>
+                        <img src={orderIcon} alt="Order Icon" />
+                        <p className="subtext">
+                            Select which type of assistance or request you need. Fill out the given
+                            form properly and make sure your account is verified.
+                        </p>
+                    </div>
+                </div>
+
+                <div className="steps__item">
+                    <h3>Wait for Approval</h3>
+                    <div>
+                        <img src={approvedIcon} alt="Approved Icon" />
+                        <p className="subtext">
+                            Your account and request will be reviewed if your request is valid and
+                            you are eligible.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
+
+export default HowItWorks;
