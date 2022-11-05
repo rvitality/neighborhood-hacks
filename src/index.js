3fed59b55a46462a20a22563d3994ebbc772ab64
rvitality <vital.renzjohn@gmail.com>
2022-11-05 22:42:18 +0800

    feat: wrap the App component with the contexts' provider

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./styles/mixins.scss";
 import "./index.scss";
 import App from "./App";
+
+import { AuthContextProvider } from "./context/AuthContext";
+import { RequestsContextProvider } from "./context/RequestsContext";
 
 import { BrowserRouter } from "react-router-dom";
 
@@ -10,7 +12,11 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
     <React.StrictMode>
         <BrowserRouter>
-            <App />
+            <AuthContextProvider>
+                <RequestsContextProvider>
+                    <App />
+                </RequestsContextProvider>
+            </AuthContextProvider>
         </BrowserRouter>
     </React.StrictMode>
 );
