3c6b2dae62676599b862c4e446efe81d05ac5962
rvitality <vital.renzjohn@gmail.com>
2022-11-06 14:03:07 +0800

    feat: add route restriction if user is not authenticated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import Header from "./routes/Header/Header.route";
 import Home from "./routes/Home/Home.route";
@@ -8,16 +8,26 @@ import Request from "./routes/Request/Request.route";
 import Medicine from "./routes/Request/Medicine/Medicine.route";
 import Admin from "./routes/Admin/Admin.route";
 import MedicineRequests from "./routes/Admin/MedicineRequests/MedicineRequests.route";
+import Staff from "./routes/Admin/Staff/Staff.route";
+import Account from "./routes/Account/Account.route";
+import { useAuthContext } from "./context/AuthContext";
 
 const App = () => {
+    const { isLoggedIn } = useAuthContext();
+
     return (
         <Routes>
             <Route path="/" element={<Header />}>
                 <Route index element={<Home />} />
-                <Route path="/request" element={<Request />} />
-                <Route path="/request/medicine" element={<Medicine />} />
-                <Route path="/admin" element={<Admin />}>
+                <Route path="/account" element={isLoggedIn ? <Account /> : <Navigate to="/" />} />
+                <Route path="/request" element={isLoggedIn ? <Request /> : <Navigate to="" />} />
+                <Route
+                    path="/request/medicine"
+                    element={isLoggedIn ? <Medicine /> : <Navigate tp="/" />}
+                />
+                <Route path="/admin" element={isLoggedIn ? <Admin /> : <Navigate to="/" />}>
                     <Route path="medicine_requests" element={<MedicineRequests />} />
+                    <Route path="staff" element={<Staff />} />
                 </Route>
             </Route>
 
